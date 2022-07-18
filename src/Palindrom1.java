460b88913a587ae568e478b2fda68be06fa240ea
praveenbondili <praveennztc@gmail.com>
2022-07-18 15:56:15 +1200

    01-18Jul2022-1533

---
+++ src/Palindrom1.java
@@ -0,0 +1,18 @@
+class Palindrome{
+    public static void main(String args[]){
+        int r,sum=0,temp;
+        int n=454;//It is the number variable to be checked for palindrome
+        int a = n;
+
+        temp=n;
+        while(n>0){
+            r=n%10;  //getting remainder
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum)
+           // System.out.println("palindrome number" + n);
+            System.out.println("Palindrome   " + a);
+        else
+            System.out.println("not palindrome");
+    }}
